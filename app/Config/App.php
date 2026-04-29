5ec289c67c3cb32a4c8329567ceb87e3cead9a2b
Lartixx <lukasmarcanik7@gmail.com>
2026-04-29 09:21:51 +0200

    zakladPrvniStranky

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/marcanik/cyklistikaProjekt/';
+    public string $baseURL = 'http://localhost/marcanik/projektSpolecne/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
---
+++ app/Controllers/Main.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\RaceYear;
+
+class Main extends BaseController
+{   
+    protected RaceYear $raceYearModel;
+
+    public function __construct()
+    {
+        $this->raceYearModel = new RaceYear();
+    }
+
+    public function index()
+    {
+        $data = $this->raceYearModel->where('sex', 'W')->orderBy('year', 'DESC')->first();
+
+        return view ('index', [
+            'data' => $data
+        ]);
+    }
+}
